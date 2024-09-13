bada2569d806d50e81276a03bb4495d1d9f357e4
Leo <46990198+DrakonianMight@users.noreply.github.com>
2024-09-13 18:37:46 +1000

    removed debug mode

--- docker/app.py
+++ docker/app.py
@@ -224,4 +224,4 @@ def update_dropdown(clickData):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run_server(debug=False, host='0.0.0.0', port=8050)
